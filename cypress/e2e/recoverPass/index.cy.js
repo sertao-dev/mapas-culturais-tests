8d03f77ed26b52130ec22a27bc7cecde8844746b
Sara Camilo <124682717+saracamilodev@users.noreply.github.com>
2024-02-21 13:44:50 -0300

    testado email para o esqueci minha senha (#9)

--- cypress/e2e/recoverPass/index.cy.js
+++ cypress/e2e/recoverPass/index.cy.js
@@ -1,14 +1,22 @@
-describe("RecoverPassword", () => {
+describe("LoginPage", () => {
   beforeEach(() => {
     cy.visit("/");
   });
 
   it("clicks the link \"Entrar\"", () => {
     cy.contains("Entrar").click();
+    cy.url().should("include", "/autenticacao/");
+  });
+});
+
+describe("RecoverPassword", () => {
+  beforeEach(() => {
+    cy.visit("/autenticacao/");
   });
 
-  it("clicks the link \"Esqueci minha senha\"", () => {
-    cy.contains("Entrar").click();
+  it("clicks the link \"Esqueci minha senha\", preenche o input email e click no botão \"Alterar senha\"", () => {
     cy.get("#multiple-login-recover").click();
+    cy.get("#email").type("seuemail@email.com");
+    cy.contains("Alterar senha").click();
   });
 });
