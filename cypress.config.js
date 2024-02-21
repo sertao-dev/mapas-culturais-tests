d6d4855e1818dfb3f93c7d89733e8c00ef1fa2fc
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2024-02-21 09:50:19 -0300

    Configure recaptcha fake

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://redesign.testes.map.as',
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
-
     },
   },
 });
