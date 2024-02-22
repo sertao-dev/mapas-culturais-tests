7a69be12f6e715e0a42d2ba88a0db3760eb4cbb1
Ana Kelly Moura Balbino <101601082+Akellymourab@users.noreply.github.com>
2024-02-22 15:39:41 -0300

    Teste dos botoes "Ver todos" da segunda seção da Homepage (#13)

--- cypress/e2e/homepage/compact.cy.js
+++ cypress/e2e/homepage/compact.cy.js
@@ -1,3 +1,5 @@
+import { backHomepageCompact } from "../../commands/backHomepageCompact";
+
 describe("Homepage compactada", () => {
   beforeEach(() => {
     cy.viewport(1000, 768);
@@ -5,8 +7,27 @@ describe("Homepage compactada", () => {
   });
 
   it("Garante o funcionamento do home", () => {
-    cy.get(".mc-header-menu__btn-mobile").click();
-    cy.contains(".mc-header-menu__itens a", "Home").click();
-    cy.url().should("include", "");
+    backHomepageCompact();
+  });
+
+  it("Acessa \"Ver todos\" de Eventos", () => {
+    cy.get(":nth-child(2) > .card__right > .button").click();
+    cy.url().should("include", "/eventos/");
+    backHomepageCompact();
+  });
+  it("Acessa \"Ver todos\" de Espaços", () => {
+    cy.get(":nth-child(3) > .card__right > .button").click();
+    cy.url().should("include", "/espacos/");
+    backHomepageCompact();
+  });
+  it("Acessa \"Ver todos\" de Agentes", () => {
+    cy.get(":nth-child(4) > .card__right > .button").click();
+    cy.url().should("include", "/agentes/");
+    backHomepageCompact();
+  });
+  it("Acessa \"Ver todos\" de Projetos", () => {
+    cy.get(":nth-child(5) > .card__right > .button").click();
+    cy.url().should("include", "/projetos/");
+    backHomepageCompact();
   });
 });
